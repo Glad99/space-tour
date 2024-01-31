e727a7d85251433105d1331be827564a2f075a8d
Gladys <gladysalu1@gmail.com>
2024-01-31 21:53:51 +0100

    commit

--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -2,7 +2,25 @@
 
 const Technology = () => {
   return (
-    <div>Technology</div>
+    <div>
+      <div className="container">
+        <div>
+          <p><span>30</span>SPACE LAUNCH 101</p>
+          <div>
+            <div>
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+            </div>
+            <div>
+              <p>THE TERMINOLOGY</p>
+              <h1>LAUNCH VEHICLE</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus sequi ea quae iusto corporis perspiciatis. Alias eveniet, repellat itaque, ea quaerat architecto dolorem excepturi quam, accusantium animi possimus temporibus molestias.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
